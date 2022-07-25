f4fd9f7abbcd0ad7eeede0cf1c098b2768d487e4
minsuk1 <palt1230@naver.com>
2022-07-25 10:07:42 +0900

    Update minsuk_app.py

--- streamlit/minsuk_app.py
+++ streamlit/minsuk_app.py
@@ -47,12 +47,23 @@ import streamlit as st
 from streamlit_folium import st_folium
 import folium
 
-# center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], 
-    popup="Liberty Bell", 
-    tooltip="Liberty Bell"
+# # center on Liberty Bell, add marker
+# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+# folium.Marker(
+#     location=[37.559819, 126.963895], 
+#     popup="Liberty Bell", 
+#     tooltip="Liberty Bell"
+# ).add_to(m)
+
+m = folium.Map(
+    location=[37.559819, 126.963895],
+    zoom_start=11, 
+    tiles='cartodbpositron'
+)
+
+folium.GeoJson(
+    seoul_geo,
+    name='지역구'
 ).add_to(m)
 
 # call to render Folium map in Streamlit
