a857d15d959d2e09ea2f767a3fb4ec9921907b79
minsuk1 <palt1230@naver.com>
2022-07-25 16:50:45 +0900

    Update minsuk_app.py

--- streamlit/minsuk_app.py
+++ streamlit/minsuk_app.py
@@ -62,8 +62,8 @@ df3 = df2[df3[0] == job_filter]
 print(df3)
 print(df3[1])
 col1, col2, col3 = st.columns(3)
-col1.metric("강남구 영업점 수", str(df3[1])+"개", "4개")
-col2.metric("총 인구 수", "11,000명", "-1,000명")
+col1.metric("강남구 영업점 수", df3[1], "4개")
+col2.metric("총 인구 수", df3[2], "-1,000명")
 col3.metric("종합점수", "86점", "5")
 
 import streamlit as st
